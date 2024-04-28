77b9772094aab132ed4ec95a13c3a7bae2b9d61b
vikashjman <vikashkumar@jmangroup.com>
2024-04-28 14:20:17 +0530

    feat(breaking changes): rename the core from newbie to web

---
+++ packages/newbie/src/app/pricing-column/page.tsx
@@ -0,0 +1,11 @@
+const PricingColumnPage = () => {
+    return (
+        <div className="h-screen gap-0 flex items-center justify-center">
+            <div>
+                
+            </div>
+        </div>
+    );
+}
+
+export default PricingColumnPage;
